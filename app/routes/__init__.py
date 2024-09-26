6cb2e58ca30c4602b526155c621b688ee2186c43
Robotz213 <nicholasazs213@gmail.com>
2024-09-26 23:02:58 +0000

    att

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,6 +1,32 @@
-from app import app
+from flask_socketio import emit, join_room, leave_room
+from app import app, socketio
 
 from app.routes.bot import bot
-
-
 app.register_blueprint(bot)
+
+@socketio.on('connect', namespace='/log')
+def handle_connect():
+    pass
+
+@socketio.on('disconnect', namespace='/log')
+def handle_disconnect():
+    pass
+
+@socketio.on('join', namespace='/log')
+def handle_join(data):
+    room = data['pid']
+    join_room(room)
+    # print(f"Client {request.sid} joined room {room}")
+
+@socketio.on('leave', namespace='/log')
+def handle_leave(data):
+    room = data['pid']
+    leave_room(room)
+    # print(f"Client {request.sid} left room {room}")
+
+@socketio.on('log_message', namespace='/log')
+def handle_message(data):
+    pid = data['pid']
+    message = data['message']
+    emit('log_message', {'message': message, 'pid': pid}, room=pid)
+    # print("mensagem enviada")
