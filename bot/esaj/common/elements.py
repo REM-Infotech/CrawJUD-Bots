e2b0655091e3a1e26147f1ce6364d41d634dfe19
Robotz213 <nicholasazs213@gmail.com>
2024-10-10 17:35:05 -0400

    att

--- bot/esaj/common/elements.py
+++ bot/esaj/common/elements.py
@@ -185,7 +185,12 @@ class elements_esaj:
 
                 
     def __call__(self, *args, **kwds) -> Union[SP, AC, AM]:
-        """"""
+        """
+        ### Retorna self.state_class
+        
+        #### self.state_class: Union[SP, AC, AM]
+        
+        """
         return self.state_class
         
     def __getattr__(self, nome_do_atributo: str) -> Any:
