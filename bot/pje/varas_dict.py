2c4a7d2f68bd1a50ea3de0094dc55c434fa5b69e
Robotz213 <nicholasazs213@gmail.com>
2024-10-11 18:02:38 -0400

    att

--- bot/pje/varas_dict.py
+++ bot/pje/varas_dict.py
@@ -1,28 +1,3 @@
-
-def varas() -> dict:
-    
-    return {
-    "1ª Vara do Trabalho de Manaus": "#VT1-1",
-    "2ª Vara do Trabalho de Manaus": "#VT2-1",
-    "3ª Vara do Trabalho de Manaus": "#VT3-1",
-    "4ª Vara do Trabalho de Manaus": "#VT4-1",
-    "5ª Vara do Trabalho de Manaus": "#VT5-1",
-    "6ª Vara do Trabalho de Manaus": "#VT6-1",
-    "7ª Vara do Trabalho de Manaus": "#VT7-1",
-    "8ª Vara do Trabalho de Manaus": "#VT8-1",
-    "9ª Vara do Trabalho de Manaus": "#VT9-1",
-    "10ª Vara do Trabalho de Manaus": "#VT10-1",
-    "11ª Vara do Trabalho de Manaus": "#VT11-1",
-    "12ª Vara do Trabalho de Manaus": "#VT12-1",
-    "13ª Vara do Trabalho de Manaus": "#VT13-1",
-    "14ª Vara do Trabalho de Manaus": "#VT14-1",
-    "15ª Vara do Trabalho de Manaus": "#VT15-1",
-    "16ª Vara do Trabalho de Manaus": "#VT16-1",
-    "17ª Vara do Trabalho de Manaus": "#VT17-1",
-    "18ª Vara do Trabalho de Manaus": "#VT18-1",
-    "19ª Vara do Trabalho de Manaus": "#VT19-1"
-}
-    
 def varas() -> dict:
     
     return {
