e7dd722309b2da1f2303decea786f1d23d956194
Robotz213 <nicholasazs213@gmail.com>
2024-10-28 12:42:56 -0400

    att

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -9,6 +9,8 @@ from app.models.secondaries import admins, execution_bots
 from app.models.users import Users, LicensesUsers
 from app.models.bots import BotsCrawJUD, Credentials, Executions, CacheLogs, ThreadBots
 
+__all__ = [admins, execution_bots, Users, LicensesUsers, BotsCrawJUD,
+           Credentials, Executions, CacheLogs, ThreadBots, Servers]
 
 
 class init_database:
